1d2295e78d76d603131177ec1ec4c8c64ffbbc37
yappo <yappo@shibuya.pl>
2009-11-05 12:53:28 +0900

    return this for all jquery plugin methods

--- lib/JSTAPd/Server/Contents.pm
+++ lib/JSTAPd/Server/Contents.pm
@@ -151,6 +151,7 @@ sub jquery_jstapd {
 
 $.fn.is_visible = function(num){
     window.is($($(this).selector+':visible').length, num, $(this).selector + ' is visible ' + num + ' items');
+    return this;
 };
 
 $.fn.isnt_visible = function(){
@@ -159,50 +160,62 @@ $.fn.isnt_visible = function(){
         ret = 1;
     }
     window.ok(ret, $(this).selector + ' is not visible');
+    return this;
 };
 
 $.fn.is_text = function(val){
     window.is($(this).text(), val, $(this).selector + " text() is '" + val + "'");
+    return this;
 };
 
 $.fn.isnt_text = function(val){
     window.isnt($(this).text(), val, $(this).selector + " text() is not '" + val + "'");
+    return this;
 };
 
 $.fn.like_text = function(val){
     window.like($(this).text(), val, $(this).selector + " text() like '" + val.toString() + "'");
+    return this;
 };
 
 $.fn.unlike_text = function(val){
     window.unlike($(this).text(), val, $(this).selector + " text() unlike '" + val.toString() + "'");
+    return this;
 };
 
 $.fn.is_formval = function(val){
     window.is($(this).val(), val, $(this).selector + " form val() is '" + val + "'");
+    return this;
 };
 
 $.fn.isnt_formval = function(val){
     window.isnt($(this).val(), val, $(this).selector + " form val() is not '" + val + "'");
+    return this;
 };
 
 $.fn.like_formval = function(val){
     window.like($(this).val(), val, $(this).selector + " form val() like '" + val.toString() + "'");
+    return this;
 };
 
 $.fn.unlike_formval = function(val){
     window.unlike($(this).val(), val, $(this).selector + " form val() unlike '" + val.toString() + "'");
+    return this;
 };
 
 $.fn.is_attr = function(name, val){
     window.is($(this).attr(name), val, $(this).selector + " '" + name + "' attr is '" + val + "'");
+    return this;
 };
 
 $.fn.ok_hasClass = function(val){
     window.ok($(this).hasClass(val), $(this).selector + " hasClass '" + val + "'");
+    return this;
 };
 
 $.fn.has_items_of = function(val){
     window.is($(this).length, val, $(this).selector + " has items of " + val + "");
+    return this;
 };
 
 })(jQuery);
